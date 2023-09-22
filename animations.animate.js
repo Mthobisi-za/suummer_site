7fd56fff89f1185f25d13aff713cab8de0868b93
Mthobisi-za <mthobisimtho333@gmail.com>
2023-09-22 08:03:51 +0200

    done

--- animations.animate.js
+++ animations.animate.js
@@ -1,91 +1,137 @@
-// let tl = gsap.timeline({
-//     // yes, we can add it to an entire timeline!
-//     scrollTrigger: {
-//         trigger: ".body",
-//         opacity: 0,
-//         pin: true, // pin the trigger element while active
-//         start: "top top", // when the top of the trigger hits the top of the viewport
-//         end: "+=500", // end after scrolling 500px beyond the start
-//         scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-//         snap: {
-//             snapTo: "labels", // snap to the closest label in the timeline
-//             duration: { min: 0.2, max: 3 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
-//             delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
-//             ease: "power1.inOut" // the ease of the snap animation ("power3" by default)
-//         }
-//     }
-// });
+gsap.from(".banner_holder", {
+    scrollTrigger: {
+        trigger: ".body",
+        // start: "50% 50%",
+        // toggleActions: "restart none none reverse"
+    }, // start the animation when ".box" enters the viewport (once)
+    opacity: 0,
+    duration: 2,
+    y: -50
+});
+gsap.from(".title_holder", {
+    scrollTrigger: {
+        trigger: ".body",
+        // start: "50% 50%",
+        // toggleActions: "restart none none reverse"
+    }, // start the animation when ".box" enters the viewport (once)
+    opacity: 0,
+    duration: 2,
+    y: 50
+});
 gsap.from(".first_anime_text", {
     scrollTrigger: {
         trigger: ".title_subline",
         start: "50% 50%",
-        toggleActions: "restart none none reverse"
+        // toggleActions: "restart none none reverse"
     }, // start the animation when ".box" enters the viewport (once)
     opacity: 0,
-    duration: 2,
+    duration: 1,
+    y: 10
 });
 
 gsap.from(".second_anime_text", {
     scrollTrigger: {
         trigger: ".title_subline",
         start: "top center",
-        toggleActions: "restart none none reverse"
+        // toggleActions: "restart none none reverse"
     }, // start the animation when ".box" enters the viewport (once)
     opacity: 0,
     duration: 2,
-    x: -50
+    y: 10
 });
+
+// fade in
 gsap.from(".prices", {
     scrollTrigger: {
-        trigger: ".first_anime_text",
-        start: "bottom center",
-        toggleActions: "restart none none reverse"
+        trigger: ".title_subline",
+        start: "top center",
+        // toggleActions: "restart none none reverse"
     }, // start the animation when ".box" enters the viewport (once)
     opacity: 0,
     duration: 2,
-    x: -50,
-    scale: 0.7,
-    scrub: 1
+    y: 10
 });
 
 gsap.from(".third_anime_text", {
     scrollTrigger: {
-        trigger: ".first_anime_text",
-        start: "bottom center",
-        toggleActions: "restart none none reverse"
-    }, // start the animation when ".box" enters the viewport (once)
-    opacity: 0,
-    duration: 2,
-    x: -50,
-    scale: 0,
-    scrub: .5
-});
-
-gsap.from(".list_items", {
-    scrollTrigger: {
-        trigger: ".grid_2nd_template",
-        start: "top top",
-        toggleActions: "restart none none reverse"
-    }, // start the animation when ".box" enters the viewport (once)
-    opacity: 0,
-    duration: 1.5,
-    x: -50,
-    y: -50,
-    scale: 0,
-    scrub: .5,
-    stagger: .2
-});
-gsap.from(".cans", {
-    scrollTrigger: {
-        trigger: ".grid_2nd_template",
-        start: "top top",
-        toggleActions: "restart none none reverse"
+        trigger: ".first_group",
+        start: "50% 50%",
+        // toggleActions: "restart none none reverse"
     }, // start the animation when ".box" enters the viewport (once)
     opacity: 0,
     duration: 1,
-    x: -50,
-    y: -50,
-    scale: 1.5,
-    scrub: .5,
-    stagger: .2
+    y: 10
+});
+
+gsap.from(".fourth_anime_text", {
+    scrollTrigger: {
+        trigger: ".first_group",
+        start: "50% 50%",
+        // toggleActions: "restart none none reverse"
+    }, // start the animation when ".box" enters the viewport (once)
+    opacity: 0,
+    duration: 2,
+    y: 10
+});
+
+gsap.from(".first_group_list", {
+    scrollTrigger: {
+        trigger: ".container_second",
+        start: "50% 50%",
+        // toggleActions: "restart none none reverse"
+    }, // start the animation when ".box" enters the viewport (once)
+    opacity: 0.5,
+    duration: 1.5,
+    y: 10
+});
+gsap.from(".second_group_list", {
+    scrollTrigger: {
+        trigger: ".container_second",
+        start: "50% 50%",
+        // toggleActions: "restart none none reverse"
+    }, // start the animation when ".box" enters the viewport (once)
+    opacity: 0.5,
+    duration: 1,
+    y: 15
+});
+gsap.from(".third_group_list", {
+    scrollTrigger: {
+        trigger: ".container_second",
+        start: "50% 50%",
+        // toggleActions: "restart none none reverse"
+    }, // start the animation when ".box" enters the viewport (once)
+    opacity: 0.5,
+    duration: 0.5,
+    y: 20
+});
+gsap.from(".extra_txt", {
+    scrollTrigger: {
+        trigger: ".container_second",
+        start: "50% 50%",
+        // toggleActions: "restart none none reverse"
+    }, // start the animation when ".box" enters the viewport (once)
+    opacity: 0,
+    duration: 1,
+    y: 10
+});
+gsap.from(".fifth_anime_text", {
+    scrollTrigger: {
+        trigger: ".first_group_list",
+        start: "50% 50%",
+        // toggleActions: "restart none none reverse"
+    }, // start the animation when ".box" enters the viewport (once)
+    opacity: 0,
+    duration: 1,
+    y: 10
+});
+
+gsap.from(".sixth_anime_text", {
+    scrollTrigger: {
+        trigger: ".cans",
+        start: "bottom bottom",
+        // toggleActions: "restart none none reverse"
+    }, // start the animation when ".box" enters the viewport (once)
+    opacity: 0,
+    duration: 1,
+    y: 10
 });
